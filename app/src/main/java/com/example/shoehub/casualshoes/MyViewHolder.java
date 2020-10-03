267148c25a3d4b9aca5672149bbe29b6045a20e0
Saranga Ranasinghe <67331592+SarangaRanasinghe@users.noreply.github.com>
2020-10-03 13:26:06 -0800

    added formalshoes part

--- app/src/main/java/com/example/shoehub/casualshoes/MyViewHolder.java
+++ app/src/main/java/com/example/shoehub/casualshoes/MyViewHolder.java
@@ -14,9 +14,9 @@ public class MyViewHolder extends RecyclerView.ViewHolder {
    ImageView profilePic;
     public MyViewHolder(@NonNull View itemView) {
         super(itemView);
-        textViewID=itemView.findViewById(R.id.AtextView1ID);
-        getTextViewName=itemView.findViewById(R.id.AtextView2ID);
-        profilePic = itemView.findViewById(R.id.Acard_image);
+        textViewID=itemView.findViewById(R.id.AtextView1ID_formal);
+        getTextViewName=itemView.findViewById(R.id.AtextView2ID_formal);
+        profilePic = itemView.findViewById(R.id.Acard_image_formal);
 
 
     }
